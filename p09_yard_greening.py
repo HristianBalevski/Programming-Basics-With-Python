9d5d13a201e6a3e876760528105ca78b15769844
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-09-23 08:57:46 +0100

    Delete p09_yard_greening.py

--- p09_yard_greening.py
+++
@@ -1,8 +0,0 @@
-sq_meters = float(input())
-
-price = sq_meters * 7.61
-discount = price * 0.18
-final_price = price - discount
-
-print(f'The final price is: {final_price} lv.')
-print(f'The discount is: {discount} lv.')
