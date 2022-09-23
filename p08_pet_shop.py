ff41276d49be9a8bef08a829a70b8a9d5f4997b3
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-09-23 08:57:55 +0100

    Delete p08_pet_shop.py

--- p08_pet_shop.py
+++
@@ -1,9 +0,0 @@
-dog_food_cnt = int(input())
-cat_food_cnt = int(input())
-
-dog_food = dog_food_cnt * 2.50
-cat_food = cat_food_cnt * 4
-
-final_price = dog_food + cat_food
-
-print(f'{final_price} lv.')
