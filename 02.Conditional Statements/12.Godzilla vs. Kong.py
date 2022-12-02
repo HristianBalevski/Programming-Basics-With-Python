1c7961d4412fb43663d11ae9c7f6dbb865decc7e
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 14:43:30 +0000

    Godzilla vs. Kong

--- 02.Conditional Statements/12.Godzilla vs. Kong.py
+++ 02.Conditional Statements/12.Godzilla vs. Kong.py
@@ -6,7 +6,6 @@ decor = budjet * 0.10
 price_for_clothes = people_cnt * price_for_clothes_1_person
 total_money = price_for_clothes + decor
 
-
 if people_cnt > 150:
 
     discount = price_for_clothes * 0.10
@@ -17,7 +16,6 @@ if total_money > budjet:
     needed_money = total_money - budjet
     print('Not enough money!')
     print(f'Wingard needs {needed_money:.2f} leva more.')
-
 else:
     money_left = budjet - total_money
     print('Action!')
