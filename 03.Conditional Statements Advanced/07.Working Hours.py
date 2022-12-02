9626295cebc13518eebcf70b4ca71b4e862b5931
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 14:58:03 +0000

    Working Hours

---
+++ 03.Conditional Statements Advanced/07.Working Hours.py
@@ -0,0 +1,41 @@
+hour = int(input())
+day = input()
+
+if day == 'Monday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Tuesday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Wednesday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Thursday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Friday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Saturday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+
+if day == 'Sunday':
+    print('closed')
