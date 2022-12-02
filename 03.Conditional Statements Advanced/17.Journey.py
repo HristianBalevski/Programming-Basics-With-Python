33a3962be9877e01d92e9986e23c76458f0cf94e
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 15:21:47 +0000

    Journey

---
+++ 03.Conditional Statements Advanced/17.Journey.py
@@ -0,0 +1,36 @@
+budget = float(input())
+season = input()
+
+destination = ''
+money = 0
+sleeping = ''
+
+if budget <= 100:
+    destination = 'Bulgaria'
+
+    if season == 'summer':
+        money = budget * 0.30
+        sleeping = 'Camp'
+
+    elif season == 'winter':
+        money = budget * 0.70
+        sleeping = 'Hotel'
+
+elif budget <= 1000:
+    destination = 'Balkans'
+
+    if season == 'summer':
+        money = budget * 0.40
+        sleeping = 'Camp'
+
+    elif season == 'winter':
+        money = budget * 0.80
+        sleeping = 'Hotel'
+
+elif budget > 1000:
+    destination = 'Europe'
+    money = budget * 0.90
+    sleeping = 'Hotel'
+
+print(f'Somewhere in {destination}')
+print(f'{sleeping} - {money:.2f}')
