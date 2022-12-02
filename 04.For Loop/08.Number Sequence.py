b383a0d19e5516f3b8e84e86a06470d939a52a2b
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 15:34:05 +0000

    Number Sequence

--- 04.For Loop/08.Number Sequence.py
+++ 04.For Loop/08.Number Sequence.py
@@ -9,6 +9,7 @@ for _ in range(number):
 
     if current_number > max_number:
         max_number = current_number
+        
     if current_number < min_number:
         min_number = current_number
 print(f'Max number: {max_number}')
