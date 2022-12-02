9fb96d43582b5244033a4efce474a8d2ddbd3362
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 15:42:15 +0000

    Old Books

--- 05.While Loop/09.Old Books.py
+++ 05.While Loop/09.Old Books.py
@@ -1,1 +1,14 @@
+name_of_book = input()
+current_book = input()
+counter = 0
 
+while current_book != 'No More Books':
+
+    if current_book == name_of_book:
+        print(f'You checked {counter} books and found it.')
+        break
+    counter += 1
+    current_book = input()
+else:
+    print('The book you search is not here!')
+    print(f'You checked {counter} books.')
