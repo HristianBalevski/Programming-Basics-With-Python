05de0f21fc2753af540d5de999652003c9747215
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-02 15:35:25 +0000

    Left And Right Sum

--- 04.For Loop/09.Left And Right Sum.py
+++ 04.For Loop/09.Left And Right Sum.py
@@ -5,10 +5,12 @@ right_sum = 0
 
 for i in range(1, number + 1):
     current_number = int(input())
-    left_sum += current_number   
+    left_sum += current_number
+    
 for i in range(1, number + 1):
     current_number = int(input())
     right_sum += current_number
+    
 if left_sum == right_sum:
     print(f'Yes, sum = {left_sum}')
 else:
